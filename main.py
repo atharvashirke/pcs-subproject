36b2dda813ae9d25b9daab7f82b865ca3b2d2c3f
Atharva Shirke <atharvashirke@berkeley.edu>
2022-04-11 20:29:59 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Write a script that takes files from data and turns them into rows for a pandas dataframe
+
+# Goal: For every bill in data, create a row in a pandas dataframe with details such as "bill text", "date-created", "authors", etc.
+
+# Tips: use string methods and regex for parsing. 
